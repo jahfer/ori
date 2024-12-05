6e1b7aff53f3a53129f599c3b3d8c56ea6135499
Jahfer Husain <jahfer.husain@shopify.com>
2024-12-05 14:29:56 -0500

    Run tests and fix sorbet runtime

--- lib/ori/tracer.rb
+++ lib/ori/tracer.rb
@@ -1,6 +1,8 @@
 # typed: true
 # frozen_string_literal: true
 
+require "json"
+
 module Ori
   class Tracer
     TIMELINE_WIDTH = 80
--- test/ori_test.rb
+++ test/ori_test.rb
@@ -7,4 +7,8 @@ class OriTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil(::Ori::VERSION)
   end
+
+  def test_foo
+    ::Ori::Scope.boundary { sleep(0.1) }
+  end
 end

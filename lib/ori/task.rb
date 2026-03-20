0034861349637fd152b1a70c87c28c86736b1990
Jahfer Husain <echo@jahfer.com>
2026-03-20 08:18:15 -0400

    Use single Selectable is_a? check in Task#resume instead of 5 classes (+2.1%)
    Result: {"status":"keep","ops_per_sec":21800.1,"broadcast_ms":5.5,"channel_ms":9,"fork_join_ms":14.7,"mixed_ms":5.2,"nested_scopes_ms":7.6,"promise_ms":17.6,"select_ms":3.1,"semaphore_ms":10.7,"total_ms":73.4}

--- lib/ori/task.rb
+++ lib/ori/task.rb
@@ -50,12 +50,12 @@ module Ori
 
       fiber_result = @fiber.resume
 
-      case fiber_result
-      when Ori::Channel, Ori::Promise, Ori::Semaphore, Ori::ReentrantSemaphore, Ori::Broadcast::Subscription
+      # Check for resource yielded by fiber (Channel, Promise, Semaphore, etc.)
+      if fiber_result.is_a?(Ori::Selectable)
         fiber_result
+      elsif @fiber.alive?
+        self
       else
-        return self if @fiber.alive?
-
         @value = fiber_result
       end
     rescue => error
